6a1a0309ed27cf9180f4ccd6d887487b820f6765
AlexNK001 <126164280+AlexNK001@users.noreply.github.com>
2024-09-16 14:02:09 +0300

    correction 1

--- CS Junior Home Work/Program.cs
+++ CS Junior Home Work/Program.cs
@@ -1,14 +1,17 @@
 ﻿public class Program
 {
-    private string _name = "Александр";
-    private string _surname = "Новицкий";
-    private string _patronymic = "Камилевич";
-    private ushort _yearBirth = 1992;
-    private int _age = 32;
-    private float _height = 1.76f;
-    private bool _haveChildrens = true;
-    private byte _numberChildren = 1;
-    private char _drivingLicenseCategory = 'B';
-    private byte _floorResidence = 9;
+    static void Main(string[] args)
+    {
+        string name = "Александр";
+        string surname = "Новицкий";
+        string patronymic = "Камилевич";
+        ushort yearBirth = 1992;
+        int age = 32;
+        float height = 1.76f;
+        bool haveChildrens = true;
+        byte numberChildren = 1;
+        char drivingLicenseCategory = 'B';
+        byte floorResidence = 9;
+    }
 }
 
