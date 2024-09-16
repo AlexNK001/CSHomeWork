f89b082c084a497d3c7529160cf4ce733986fc74
AlexNK001 <126164280+AlexNK001@users.noreply.github.com>
2024-09-16 15:05:02 +0300

    Permutation of values

---
+++ Permutation of values/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Permutation_of_values
+{
+    class Program
+    {
+        static void Main()
+        {
+            int firstNumber = 3;
+            int secondNumber = 5;
+            int temporaryNumber;
+
+            Console.WriteLine($"Первое число - {firstNumber}, второе число - {secondNumber}");
+
+            temporaryNumber = firstNumber;
+            firstNumber = secondNumber;
+            secondNumber = temporaryNumber;
+
+            Console.WriteLine($"Первое число - {firstNumber}, второе число - {secondNumber}");
+            Console.ReadKey();
+        }
+    }
+}
