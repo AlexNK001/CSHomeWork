f27d8714424209a05b78df7a6fdd16b3b041a555
AlexNK001 <126164280+AlexNK001@users.noreply.github.com>
2024-09-16 14:22:16 +0300

    Working with strings

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+public class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите Ваше имя");
+        string name = Console.ReadLine();
+        Console.Clear();
+
+        Console.Write("Введите Ваш возраст");
+        string age = Console.ReadLine();
+        Console.Clear();
+
+        Console.Write("Введите Ваше место работы");
+        string placeWork = Console.ReadLine();
+        Console.Clear();
+
+        Console.WriteLine($"Ваше имя {name}, возраст {age} и работаете в {placeWork}.");
+
+        Console.ReadKey();
+    }
+}
