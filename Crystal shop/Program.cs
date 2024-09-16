2c84fb24f15694378e2a01840aff0663b82ba6d9
AlexNK001 <126164280+AlexNK001@users.noreply.github.com>
2024-09-16 15:24:06 +0300

    Crystal shop

---
+++ Crystal shop/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Crystal_shop
+{
+    class Program
+    {
+        static void Main()
+        {
+            int priceCrystal = 3;
+
+            Console.Write("Введите количество Вашего золота");
+            int amountGold = Convert.ToInt32(Console.ReadLine());
+            Console.Clear();
+
+            Console.Write("Cколько кристаллов вы хотите купить?");
+            int amountCrystals = Convert.ToInt32(Console.ReadLine());
+            Console.Clear();
+
+            amountGold -= amountCrystals * priceCrystal;
+            Console.Write($"Золота - {amountGold}, кристалов - {amountCrystals}.");
+            Console.ReadKey();
+        }
+    }
+}
