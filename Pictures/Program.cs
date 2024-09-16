dce58b7a5bcd6f5e0ed7b3963120d5bb04f99381
AlexNK001 <126164280+AlexNK001@users.noreply.github.com>
2024-09-16 14:57:07 +0300

    Pictures

---
+++ Pictures/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Pictures
+{
+    class Program
+    {
+        static void Main()
+        {
+            int numberPictures = 52;
+            int rowCapacity = 3;
+            int filledRows = numberPictures / rowCapacity;
+            int extraPictures = numberPictures % rowCapacity;
+
+            Console.WriteLine($"Количество заполненных рядов - {filledRows}, картинок сверх меры - {extraPictures}.");
+            Console.ReadKey();
+        }
+    }
+}
