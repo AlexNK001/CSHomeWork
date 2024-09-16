3998d5ac82ee85c6865aab1ed32b12c5f9ebaad1
AlexNK001 <126164280+AlexNK001@users.noreply.github.com>
2024-09-16 16:18:16 +0300

    Polyclinic

---
+++ Polyclinic/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Polyclinic
+{
+    class Program
+    {
+        static void Main()
+        {
+            int numberMinutesInHour = 60;
+            int receptionTime = 10;
+
+            Console.Write("Введите количество людей в очереди");
+            int numberPeople = Convert.ToInt32(Console.ReadLine());
+            Console.Clear();
+
+            int allTimeInMinutes = numberPeople * receptionTime;
+            int finalTimeInMinutes = allTimeInMinutes % numberMinutesInHour;
+            int finalTimeInHours = (allTimeInMinutes - finalTimeInMinutes) / numberMinutesInHour;
+
+            Console.WriteLine($"Время ожидания {finalTimeInHours} часа и {finalTimeInMinutes} минут.");
+            Console.ReadKey();
+        }
+    }
+}
