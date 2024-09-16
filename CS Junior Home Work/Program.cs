cd8f9d5c21bac86c6022dc276a6c8d0a7253b256
AlexNK001 <126164280+AlexNK001@users.noreply.github.com>
2024-09-16 13:47:32 +0300

    init

---
+++ CS Junior Home Work/Program.cs
@@ -0,0 +1,14 @@
+﻿public class Program
+{
+    private string _name = "Александр";
+    private string _surname = "Новицкий";
+    private string _patronymic = "Камилевич";
+    private ushort _yearBirth = 1992;
+    private int _age = 32;
+    private float _height = 1.76f;
+    private bool _haveChildrens = true;
+    private byte _numberChildren = 1;
+    private char _drivingLicenseCategory = 'B';
+    private byte _floorResidence = 9;
+}
+
