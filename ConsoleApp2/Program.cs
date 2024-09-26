596882642ac4929a2b37c07329303e25b60bf897
AlexNK001 <126164280+AlexNK001@users.noreply.github.com>
2024-09-26 16:18:31 +0300

    Multiples

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Multiples
+{
+    class Program
+    {
+        static void Main()
+        {
+            int minRandomValue = 10;
+            int maxRandomValue = 25;
+            int counter = 0;
+            int minValue = 50;
+            int maxValue = 150;
+            Random random = new Random();
+            int number = random.Next(minRandomValue, maxRandomValue);
+
+            for (int i = number; i < maxValue; i += number)
+            {
+                if (i >= minValue)
+                {
+                    counter++;
+                }
+            }
+
+            Console.WriteLine(counter);
+            Console.ReadKey();
+        }
+    }
+}
