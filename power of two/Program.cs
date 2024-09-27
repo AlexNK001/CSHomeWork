dd2cbfcce24c9754a7531ff52d5bc635f3da2822
AlexNK001 <126164280+AlexNK001@users.noreply.github.com>
2024-09-27 11:06:23 +0300

    power of two

---
+++ power of two/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace power_of_two
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            int minRandomValue = 1;
+            int maxRandomValue = 28;
+            int counter = 0;
+            int minValue = 100;
+            int maxValue = 1000;
+            Random random = new Random();
+            int number = random.Next(minRandomValue, maxRandomValue);
+
+            for (int i = number; i < maxValue; i += number)
+            {
+                if (i >= minValue)
+                {
+                    counter++;
+                }
+            }
+
+            Console.WriteLine(counter);
+            Console.ReadKey();
+        }
+    }
+}
