9cd5ee4ddd316cd18f7b008d44a636f6bd47dcc8
AlexNK001 <126164280+AlexNK001@users.noreply.github.com>
2024-09-27 21:12:33 +0300

    Динамический массив
    исправление

--- Dynamic array/DynamicArray.cs
+++ Dynamic array/DynamicArray.cs
@@ -4,40 +4,42 @@ namespace Dynamic_array
 {
     internal class DynamicArray
     {
+        private const string InputSum = "sum";
+        private const string InpurExit = "exit";
+
         private static void Main()
         {
-            string inputSum = "sum";
-            string inpurExit = "exit";
             string userInput;
-            bool isProgramm = true;
+            bool isWorking = true;
             int[] numbers = new int[0];
+            int sum = 0;
 
-            while (isProgramm)
+            while (isWorking)
             {
                 userInput = Console.ReadLine();
 
-                if (userInput == inputSum)
+                switch (userInput)
                 {
-                    int sum = 0;
+                    case InputSum:
+                        for (int i = 0; i < numbers.Length; i++)
+                            sum += numbers[i];
 
-                    for (int i = 0; i < numbers.Length; i++)
-                        sum += numbers[i];
+                        Console.WriteLine(sum);
+                        break;
 
-                    Console.WriteLine(sum);
-                }
-                else if (userInput == inpurExit)
-                {
-                    isProgramm = false;
-                }
-                else
-                {
-                    int[] tempNumber = new int[numbers.Length + 1];
+                    case InpurExit:
+                        isWorking = false;
+                        break;
 
-                    for (int i = 0; i < numbers.Length; i++)
-                        tempNumber[i] = numbers[i];
+                    default:
+                        int[] tempNumbers = new int[numbers.Length + 1];
 
-                    numbers = tempNumber;
-                    numbers[numbers.Length - 1] = Convert.ToInt32(userInput);
+                        for (int i = 0; i < numbers.Length; i++)
+                            tempNumbers[i] = numbers[i];
+
+                        numbers = tempNumbers;
+                        numbers[numbers.Length - 1] = Convert.ToInt32(userInput);
+                        break;
                 }
             }
         }
