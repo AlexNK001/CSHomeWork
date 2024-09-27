e286ce9ca43d8b86deaba5d00ec253ec5546bb78
AlexNK001 <126164280+AlexNK001@users.noreply.github.com>
2024-09-27 09:51:08 +0300

    Password protected

---
+++ The program is password protected/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace The_program_is_password_protected
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            string secretMessage = "Desine sperare qui hic intras";
+            string password = "qwerty";
+            string userInput;
+            int numberAttempts = 3;
+
+            for (int i = numberAttempts; i > 0; i--)
+            {
+                Console.WriteLine($"Колличество попыток - {i}");
+                Console.Write("Для получения секретной фразы, введите пароль:");
+                userInput = Console.ReadLine();
+
+                if (userInput == password)
+                {
+                    Console.WriteLine(secretMessage);
+                    Console.ReadKey();
+                    break;
+                }
+            }
+        }
+    }
+}
