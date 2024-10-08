f208caa49892398a7bdd6ee7b1a6c08e437cfb92
AlexNK001 <126164280+AlexNK001@users.noreply.github.com>
2024-10-08 10:25:32 +0300

    init collections

---
+++ Сollections/Combination into One Collection/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Combination_into_One_Collection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
