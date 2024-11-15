484655a945298a03b32c8bec76bde5220cbde16e
AlexNK001 <126164280+AlexNK001@users.noreply.github.com>
2024-11-15 11:19:39 +0300

    Brave new world save

--- Functions/Brave new world/Program.cs
+++ Functions/Brave new world/Program.cs
@@ -11,15 +11,11 @@ namespace Brave_new_world
             const ConsoleKey DownArrowPressed = ConsoleKey.DownArrow;
             const ConsoleKey LeftArrowPressed = ConsoleKey.LeftArrow;
             const ConsoleKey RightArrowPressed = ConsoleKey.RightArrow;
-            const ConsoleKey CommandDrawingMode = ConsoleKey.Q;
-            const ConsoleKey CommandDraw = ConsoleKey.W;
             const ConsoleKey CommandExit = ConsoleKey.Escape;
 
-            int playerPositionX;
-            int playerPositionY;
-            bool canPaint = false;
+            const int FrameDuration = 200;
+
             bool isPlaying = true;
-            bool isDrawing = false;
             char playerSymbol = '@';
             char wallSymbol = '#';
             char voidSymbol = ' ';
@@ -43,117 +39,66 @@ namespace Brave_new_world
             Console.CursorVisible = false;
 
             DrawMap(map);
-            FindPlayer(map, out playerPositionX, out playerPositionY, playerSymbol);
-
-            int lastPlayerX = playerPositionX;
-            int lastPlayerY = playerPositionY;
+            FindPlayer(map, out int verticalPosition, out int horizontalPosition, playerSymbol);
 
             while (isPlaying)
             {
-                int directionX = 0;
-                int directionY = 0;
+                int nextVerticalPosition = verticalPosition;
+                int nextHorizontalPosition = horizontalPosition;
 
                 Console.SetCursorPosition(0, map.GetLength(0));
-                Console.WriteLine($"Режим рисования {canPaint}");
-                Console.WriteLine($"Для включения режима рисования нажмите {CommandDrawingMode}");
-                Console.WriteLine($"Для рисования в режиме рисования нажмите {CommandDraw}");
                 Console.WriteLine($"Для выхода нажмите {CommandExit}");
                 Console.SetCursorPosition(0, 0);
 
-                if (Console.KeyAvailable)
+                switch (Console.ReadKey(true).Key)
                 {
-                    ConsoleKeyInfo key = Console.ReadKey(true);
+                    case UpArrowPressed:
+                        nextVerticalPosition--;
+                        break;
 
-                    switch (key.Key)
+                    case DownArrowPressed:
+                        nextVerticalPosition++;
+                        break;
+
+                    case LeftArrowPressed:
+                        nextHorizontalPosition--;
+                        break;
+
+                    case RightArrowPressed:
+                        nextHorizontalPosition++;
+                        break;
+
+                    case CommandExit:
+                        isPlaying = false;
+                        break;
+                }
+
+                if (CheckBorders(nextVerticalPosition, nextHorizontalPosition, map))
+                {
+                    if (map[nextVerticalPosition, nextHorizontalPosition] != wallSymbol)
                     {
-                        case UpArrowPressed:
-                            ChoiceDirection(ref directionX, ref directionY, -1, 0);
-                            break;
-
-                        case DownArrowPressed:
-                            ChoiceDirection(ref directionX, ref directionY, 1, 0);
-                            break;
-
-                        case LeftArrowPressed:
-                            ChoiceDirection(ref directionX, ref directionY, 0, -1);
-                            break;
-
-                        case RightArrowPressed:
-                            ChoiceDirection(ref directionX, ref directionY, 0, 1);
-                            break;
-
-                        case CommandDrawingMode:
-                            EnableDrawingMode(ref canPaint, map, ref lastPlayerX, ref lastPlayerY, playerPositionX, playerPositionY, wallSymbol, voidSymbol);
-                            break;
-
-                        case CommandDraw:
-                            isDrawing = true;
-                            break;
-
-                        case CommandExit:
-                            isPlaying = false;
-                            break;
+                        DrawOldSymbol(verticalPosition, horizontalPosition, map, wallSymbol, voidSymbol);
+                        Move(ref verticalPosition, ref horizontalPosition, nextVerticalPosition, nextHorizontalPosition);
+                        DrawSymbol(verticalPosition, horizontalPosition, playerSymbol);
                     }
                 }
 
-                if (CheckBorder(playerPositionX, directionX, map, 0) && CheckBorder(playerPositionY, directionY, map, 1))
-                {
-                    if (canPaint == true && isDrawing == true)
-                    {
-                        DrawSymbol(playerPositionX, playerPositionY, wallSymbol);
-                        ReversalSymbol(map, playerPositionX, playerPositionY, wallSymbol, voidSymbol);
-                        Move(ref playerPositionX, ref playerPositionY, directionX, directionY);
-                        DeleteObsoletePosition(map, ref lastPlayerX, ref lastPlayerY, playerPositionX, playerPositionY, wallSymbol, voidSymbol);
-                        DrawMap(map);
-
-                    }
-                    else if (canPaint == true)
-                    {
-                        DrawSymbol(playerPositionX, playerPositionY, map[playerPositionX, playerPositionY]);
-                        Move(ref playerPositionX, ref playerPositionY, directionX, directionY);
-                        DrawSymbol(playerPositionX, playerPositionY, playerSymbol);
-                    }
-                    else if (map[playerPositionX + directionX, playerPositionY + directionY] != wallSymbol)
-                    {
-                        DrawOldSymbol(playerPositionX, playerPositionY, map, wallSymbol, voidSymbol);
-                        Move(ref playerPositionX, ref playerPositionY, directionX, directionY);
-                        DrawSymbol(playerPositionX, playerPositionY, playerSymbol);
-                    }
-
-                    isDrawing = false;
-                }
-
-                Thread.Sleep(200);
+                Thread.Sleep(FrameDuration);
             }
         }
 
-        static char[,] ReversalSymbol(char[,] map, int playerX, int playerY, char first, char second)
+        static bool CheckBorders(int verticalDirection, int horizontalDirection, char[,] map)
         {
-            if (map[playerX, playerY] == first)
-                map[playerX, playerY] = second;
-            else if (map[playerX, playerY] == second)
-                map[playerX, playerY] = first;
+            bool canVerticalMove = verticalDirection < map.GetLength(0) && verticalDirection >= 0;
+            bool canHorizontalMove = horizontalDirection < map.GetLength(1) && horizontalDirection >= 0;
 
-            return map;
+            return canVerticalMove && canHorizontalMove;
         }
 
-        static bool CheckBorder(int axis, int direction, char[,] map, int dimensionNumber)
+        static void Move(ref int nextVerticalPosition, ref int playerY, int directionX, int directionY)
         {
-            bool canMove = axis + direction < map.GetLength(dimensionNumber) && axis + direction >= 0;
-
-            return canMove;
-        }
-
-        static void ChoiceDirection(ref int directionX, ref int directionY, int axisX, int axisY)
-        {
-            directionX += axisX;
-            directionY += axisY;
-        }
-
-        static void Move(ref int playerX, ref int playerY, int directionX, int directionY)
-        {
-            playerX += directionX;
-            playerY += directionY;
+            nextVerticalPosition = directionX;
+            playerY = directionY;
         }
 
         static void DrawSymbol(int playerX, int playerY, char symbol)
@@ -170,25 +115,6 @@ namespace Brave_new_world
                 Console.Write(symbolWall);
             else
                 Console.Write(symbolVoid);
-        }
-
-        static void EnableDrawingMode(ref bool isPaint, char[,] map, ref int lastX, ref int lastY, int playerX, int playerY, char symbolWall, char symbolVoid)
-        {
-            if (isPaint == false)
-                isPaint = true;
-            else
-                isPaint = false;
-
-            DeleteObsoletePosition(map, ref lastX, ref lastY, playerX, playerY, symbolWall, symbolVoid);
-            DrawMap(map);
-        }
-
-        static void DeleteObsoletePosition(char[,] map, ref int lastX, ref int lastY, int playerX, int playerY, char symbolWall, char symbolVoid)
-        {
-            if (map[lastX, lastY] != symbolWall)
-                map[lastX, lastY] = symbolVoid;
-
-            lastX = playerX; lastY = playerY;
         }
 
         static void DrawMap(char[,] map)
