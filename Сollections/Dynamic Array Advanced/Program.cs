cdc758a0803762875ea2bfd532c22b00761fcb52
AlexNK001 <126164280+AlexNK001@users.noreply.github.com>
2024-11-16 17:22:33 +0300

    init Dynamic_Array_Advanced

--- Сollections/Dynamic Array Advanced/Program.cs
+++ Сollections/Dynamic Array Advanced/Program.cs
@@ -1,15 +1,47 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Dynamic_Array_Advanced
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            string commandSum = "sum";
+            string commandExit = "exit";
+            string userInput;
+            bool isProgramm = true;
+            List<int> numbers = new List<int>();
+
+            while (isProgramm)
+            {
+                userInput = Console.ReadLine();
+
+                if (userInput == commandSum)
+                    CalculateAmount(numbers);
+                else if (userInput == commandExit)
+                    isProgramm = false;
+                else
+                    AddNumber(userInput, numbers);
+            }
+        }
+
+        static void CalculateAmount(List<int> numbers)
+        {
+            int sum = 0;
+
+            foreach (int number in numbers)
+                sum += number;
+
+            Console.WriteLine(sum);
+        }
+
+        static void AddNumber(string userInput, List<int> numbers)
+        {
+            if (int.TryParse(userInput, out int number))
+                numbers.Add(number);
+            else
+                Console.WriteLine("Ошибка");
         }
     }
 }
