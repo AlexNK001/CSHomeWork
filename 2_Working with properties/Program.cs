a3e909229e9765a826e95759495a13aa41754402
AlexNK001 <126164280+AlexNK001@users.noreply.github.com>
2025-01-05 16:40:21 +0300

    test

---
+++ 2_Working with properties/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_Working_with_properties
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
